7a00cd01d9e80dbbbb7639d781a532ac3927335d
Michael Lai <mrlai7782@gmail.com>
2021-05-07 17:22:38 -0700

    update

--- index.js
+++ index.js
@@ -6,14 +6,31 @@ http
   .createServer((req, res) => {
     const q = url.parse(req.url, true);
     const filename = "." + q.pathname;
-    fs.readFile(filename, (err, data) => {
-      if (err) {
+
+    console.log(q);
+    console.log(filename);
+
+    if (filename === "./") {
+      fs.readFile("./index.html", (err, data) => {
         res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("error");
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      return res.end();
-    });
+        res.write(data);
+        return res.end();
+      });
+    } else if (
+      filename !== "./about.html" &&
+      filename !== "./contact-me.html"
+    ) {
+      fs.readFile("./404.html", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile(filename, (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
